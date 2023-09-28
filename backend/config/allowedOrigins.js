e0e6656917c185165e9edf0f3c7cf988ec2d073a
Charles Baker <charlesbaker.bakersbinary.com>
2023-09-27 19:51:22 -0700

    undo changes

--- backend/config/allowedOrigins.js
+++
@@ -1,9 +0,0 @@
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://127.0.0.1:5500',
-
-    'https://www.indigenousdatabase.com',
-    'https://indigenousdatabase.com'
-]
-
-module.exports = allowedOrigins
