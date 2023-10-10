a96c94a4fbeb7c00ee284d17f0e075fbeef514ed
Charles Baker <charlesbaker.bakersbinary.com>
2023-10-10 14:25:04 -0700

    Various UI/UX changes, added in an intro section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header/Header'
 import NavBar from './NavBar/NavBar'
 import Hero from './Hero/Hero'
+import Intro from './Intro/Intro'
 import HomeSwitcher from './HomeSwitcher/HomeSwitcher'
 import HomeBoxes from './HomeBoxes/HomeBoxes'
 import HomeBanner from './GroupBanner/GroupBanner'
@@ -17,9 +18,10 @@ const Home = () => {
                 <Header />
                 <NavBar />
                 <Hero />
+                <Intro />
                 <HomeSwitcher />
-                <HomeBanner />
                 <HomeBoxes />
+                <HomeBanner />
                 <Footer />
             </body>
         </html>
