9974fe87f583f9e7712d4693849718a2f691f057
Charles Baker <charlesbaker.bakersbinary.com>
2023-10-10 10:42:35 -0700

    Various Switcher Changes

--- frontend/src/components/HomeSwitcher/HomeSwitcher.jsx
+++ frontend/src/components/HomeSwitcher/HomeSwitcher.jsx
@@ -3,65 +3,61 @@ import HomeSwitcherCSS from './HomeSwitcher.module.css'
 import ArrowButton from './../ArrowButton/ArrowButton'
 
 const HomeSwitcher = () => {
-
-    const [icons, setIcons] = useState([
-        {source: '/assets/icon_placeholder.svg', id: 1},
-        {source: '/assets/icon_placeholder.svg', id: 2},
-        {source: '/assets/icon_placeholder.svg', id: 3},
-        {source: '/assets/icon_placeholder.svg', id: 4}
-    ])
     const content = [
         {
             id: 1,
-            image: '/assets/bgUser.jpg',
+            icon: '/assets/icon_placeholder.svg',
+            image: '/assets/books.jpg',
             name: 'Search for Books',
             text: 'Find books by indigenous groups worldwide, effortlessly find reliable sources for your research and interests.',
             button: 'books'
         },
         {
             id: 2,
-            image: '/assets/bgUser.jpg',
+            icon: '/assets/icon_placeholder.svg',
+            image: '/assets/art.jpg',
             name: 'Search for Art',
             text: 'Discover art by artists spanning generations. Dive into the diverse world of indigenous artists.',
             button: 'art'
         },
         {
             id: 3,
-            image: '/assets/bgUser.jpg',
+            icon: '/assets/icon_placeholder.svg',
+            image: '/assets/articles.jpg',
             name: 'Search for Articles',
             text: 'Step back through time and experience history through articles organized by date or topic.',
             button: 'articles'
         },
         {
             id: 4,
-            image: '/assets/bgUser.jpg',
+            icon: '/assets/icon_placeholder.svg',
+            image: '/assets/music.jpg',
             name: 'Search for Music',
             text: 'Find modern and traditional indigenous music from across multiple generations.',
             button: 'music'
         }
     ]
+
     const [view, setView] = useState(1)
     const chooseView = (id) => {
-        const newView = content.filter(content => content.id === view)
-        console.log(newView)
         setView(() => id)
     }
 
     return (
             <section className={HomeSwitcherCSS.container}>
-                <IconList icons={icons} chooseView={chooseView}></IconList>
+                <IconList content={content} chooseView={chooseView}></IconList>
                 <View content={content} view={view}/>
             </section>
     )
 }
 
-const IconList = ({ icons, chooseView }) => {
+const IconList = ({ content, chooseView }) => {
     return (
         <div className={HomeSwitcherCSS.iconList}>
-        {icons.map((icon) => {
+        {content.map((content) => {
             return (
-                <label onClick={() => chooseView(icon.id)} className={HomeSwitcherCSS.iconContainer}>
-                    <img src={icon.source}  alt="" className={HomeSwitcherCSS.icon} />
+                <label onClick={() => chooseView(content.id)} className={HomeSwitcherCSS.iconContainer}>
+                    <img src={content.icon}  alt="" className={HomeSwitcherCSS.icon} />
                 </label>
             )
         })}
@@ -71,14 +67,36 @@ const IconList = ({ icons, chooseView }) => {
 
 const View = ({ content, view }) => {
     content = content.filter(content => content.id === view)[0]
+
+    var color = `${HomeSwitcherCSS.contentContainer} ${HomeSwitcherCSS.books}`
+    switch(content.id) {
+        case 1:
+            color = `${HomeSwitcherCSS.contentContainer} ${HomeSwitcherCSS.books}`
+            break
+        case 2:
+            color = `${HomeSwitcherCSS.contentContainer} ${HomeSwitcherCSS.art}`
+            break
+        case 3:
+            color = `${HomeSwitcherCSS.contentContainer} ${HomeSwitcherCSS.articles}`
+            break
+        case 4:
+            color = `${HomeSwitcherCSS.contentContainer} ${HomeSwitcherCSS.music}`
+    }
+
     return (
-        <div className={HomeSwitcherCSS.contentContainer}>
+        <div className={color}>
             <p>{content.name}</p>
             <p>{content.text}</p>
             <ArrowButton>Start your search</ArrowButton>
-            <img className={HomeSwitcherCSS.image} src={content.image} alt="" />
+            <img 
+                className={HomeSwitcherCSS.image}
+                src={content.image} 
+                alt="" 
+            />
         </div>
     )
 }
+
+
 
 export default HomeSwitcher
